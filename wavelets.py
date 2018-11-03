5cdbca900b4dc44522d78325d13d416a4ef9bf38
Antonio Diogo Forte Martins <antdiogofmartins@gmail.com>
2018-11-03 12:15:47 -0300

    Haar Wavelet Transform components modularization.

--- wavelets.py
+++ wavelets.py
@@ -1,6 +1,7 @@
 import imageio
 import numpy as np
 from math import *
+from PIL import Image
 
 
 def haar(data):
@@ -20,6 +21,37 @@ def haar(data):
 
     for i in range(data.shape[0]):
         data[i] = temp[i]
+
+
+def haar_2D_component(im, iterations):
+    rows, columns = im.shape
+
+    for k in range(iterations):
+        level = 1 << k
+
+        level_columns = int(columns/level)
+        level_rows = int(rows/level)
+
+        row = np.zeros(level_rows)
+
+        for i in range(level_rows):
+            for j in range(row.shape[0]):
+                row[j] = im[i][j]
+
+            haar(row)
+
+            for j in range(row.shape[0]):
+                im[i][j] = row[j]
+
+        column = np.zeros(level_columns)
+        for j in range(level_columns):
+            for i in range(column.shape[0]):
+                column[i] = im[i][j]
+
+            haar(column)
+
+            for i in range(column.shape[0]):
+                im[i][j] = column[i]
 
 
 def haar_2D_grayscale(im_name, im, iterations):
@@ -58,34 +90,54 @@ def haar_2D_grayscale(im_name, im, iterations):
 def haar_2D_rgb(im_name, im, iterations):
     rows, columns, colors = im.shape
 
-    for k in range(iterations):
-        level = 1 << k
+    r = im[:, :, 0]
+    g = im[:, :, 1]
+    b = im[:, :, 2]
 
-        level_columns = int(columns/level)
-        level_rows = int(rows/level)
+    haar_2D_component(r, iterations)
+    haar_2D_component(g, iterations)
+    haar_2D_component(b, iterations)
 
-        for color in range(colors):
+    image = Image.new('RGB', (im.shape[0], im.shape[1]))
 
-            row = np.zeros(level_rows)
+    for i in range(im.shape[0]):
+        for j in range(im.shape[1]):
+            image.putpixel((j, i), (r[i][j], g[i][j], b[i][j]))
 
-            for i in range(level_rows):
-                for j in range(row.shape[0]):
-                    row[j] = im[i][j][color]
+    # for k in range(iterations):
+    #     level = 1 << k
+    #
+    #     level_columns = int(columns/level)
+    #     level_rows = int(rows/level)
+    #
+    #     for color in range(colors):
+    #
+    #         row = np.zeros(level_rows)
+    #
+    #         for i in range(level_rows):
+    #             for j in range(row.shape[0]):
+    #                 row[j] = im[i][j][color]
+    #
+    #             haar(row)
+    #
+    #             for j in range(row.shape[0]):
+    #                 im[i][j][color] = row[j]
+    #
+    #         column = np.zeros(level_columns)
+    #         for j in range(level_columns):
+    #             for i in range(column.shape[0]):
+    #                 column[i] = im[i][j][color]
+    #
+    #             haar(column)
+    #
+    #             for i in range(column.shape[0]):
+    #                 im[i][j][color] = column[i]
 
-                haar(row)
-
-                for j in range(row.shape[0]):
-                    im[i][j][color] = row[j]
-
-            column = np.zeros(level_columns)
-            for j in range(level_columns):
-                for i in range(column.shape[0]):
-                    column[i] = im[i][j][color]
-
-                haar(column)
-
-                for i in range(column.shape[0]):
-                    im[i][j][color] = column[i]
+    imageio.imwrite("post_processed_images/haarR_" + im_name, r)
+    imageio.imwrite("post_processed_images/haarG_" + im_name, g)
+    imageio.imwrite("post_processed_images/haarB_" + im_name, b)
 
     imageio.imwrite("post_processed_images/haar_" + im_name, im)
+    image.save('haar.png', "PNG")
+
 
