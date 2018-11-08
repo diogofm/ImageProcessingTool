d48ad8f4665fbd6a50b747b2c4f47872df2708b5
Antonio Diogo Forte Martins <antdiogofmartins@gmail.com>
2018-11-07 21:15:26 -0300

    Inverse Haar Wavelet Transform.

--- wavelets.py
+++ wavelets.py
@@ -4,6 +4,25 @@ from math import *
 from PIL import Image
 
 
+def haar_v2(data):
+    w0 = 0.5
+    w1 = -0.5
+    s0 = 0.5
+    s1 = 0.5
+
+    temp = np.zeros(data.shape, dtype=np.float)
+
+    h = data.shape[0] >> 1
+
+    for i in range(h):
+        k = i << 1
+        temp[i] = data[k] * s0 +  data[k + 1] * s1
+        temp[i + h] = (data[k] * w0 + data[k + 1] * w1) + 127
+
+    for i in range(data.shape[0]):
+        data[i] = temp[i]
+
+
 def haar(data):
     w0 = 0.5
     w1 = -0.5
@@ -35,8 +54,8 @@ def inverse_haar(data):
 
     for i in range(h):
         k = i << 1
-        temp[k] = (data[i] * s0 + data[i + h] * w0) / w0
-        temp[k + 1] = (data[i] * s1 + data[i + h] * w1) / s0
+        temp[k] = (data[i] * s0 + (data[i + h] - 127) * w0) / w0
+        temp[k + 1] = (data[i] * s1 + (data[i + h] - 127) * w1) / s0
 
     for i in range(data.shape[0]):
         data[i] = temp[i]
@@ -45,20 +64,24 @@ def inverse_haar(data):
 def inverse_haar_2D_component(im, iterations):
     rows, columns = im.shape
 
-    row = np.zeros(rows)
-    column = np.zeros(columns)
+    for k in range(iterations - 1, -1, -1):
+        level = 1 << k
 
-    for l in range(iterations):
-        for j in range(columns):
-            for i in range(row.shape[0]):
+        level_columns = int(columns / level)
+        level_rows = int(rows / level)
+
+        column = np.zeros(level_columns)
+        for j in range(level_columns):
+            for i in range(column.shape[0]):
                 column[i] = im[i][j]
 
             inverse_haar(column)
 
-            for i in range(row.shape[0]):
+            for i in range(column.shape[0]):
                 im[i][j] = column[i]
 
-        for i in range(rows):
+        row = np.zeros(level_rows)
+        for i in range(level_rows):
             for j in range(row.shape[0]):
                 row[j] = im[i][j]
 
@@ -71,20 +94,24 @@ def inverse_haar_2D_component(im, iterations):
 def inverse_haar_2D_grayscale(im_name, im, iterations):
     rows, columns = im.shape
 
-    row = np.zeros(rows)
-    column = np.zeros(columns)
+    for k in range(iterations-1, -1, -1):
+        level = 1 << k
 
-    for l in range(iterations):
-        for j in range(columns):
-            for i in range(row.shape[0]):
+        level_columns = int(columns / level)
+        level_rows = int(rows / level)
+
+        column = np.zeros(level_columns)
+        for j in range(level_columns):
+            for i in range(column.shape[0]):
                 column[i] = im[i][j]
 
             inverse_haar(column)
 
-            for i in range(row.shape[0]):
+            for i in range(column.shape[0]):
                 im[i][j] = column[i]
 
-        for i in range(rows):
+        row = np.zeros(level_rows)
+        for i in range(level_rows):
             for j in range(row.shape[0]):
                 row[j] = im[i][j]
 
@@ -240,3 +267,103 @@ def inverse_haar_2D_rgb(im_name, im, iterations):
 
     imageio.imwrite("post_processed_images/inverse_haar_" + im_name, im)
     image.save('inverse_haar.png', "PNG")
+
+
+def haar_2D_grayscale_v2(im_name, im, iterations):
+    rows, columns = im.shape
+
+    # for i in range(rows):
+    #     for j in range(columns):
+    #         im[i][j] = scale(0, 255, -1, 1, im[i][j])
+
+    # row = np.zeros(rows)
+    # column = np.zeros(columns)
+
+    for k in range(iterations):
+        level = 1 << k
+
+        level_columns = int(columns / level)
+        level_rows = int(rows / level)
+
+        row = np.zeros(level_rows)
+
+        for i in range(level_rows):
+            for j in range(row.shape[0]):
+                row[j] = im[i][j]
+
+            haar_v2(row)
+
+            for j in range(row.shape[0]):
+                im[i][j] = row[j]
+
+        column = np.zeros(level_columns)
+        for j in range(level_columns):
+            for i in range(column.shape[0]):
+                column[i] = im[i][j]
+
+            haar_v2(column)
+
+            for i in range(column.shape[0]):
+                im[i][j] = column[i]
+
+    # for i in range(rows):
+    #     for j in range(columns):
+    #         im[i][j] = scale(-1, 1, 0, 255, im[i][j])
+
+    imageio.imwrite("post_processed_images/haar_" + im_name, im)
+
+
+def haar_2D_component_v2(im, iterations):
+    rows, columns = im.shape
+
+    # row = np.zeros(rows)
+    # column = np.zeros(columns)
+
+    for k in range(iterations):
+        level = 1 << k
+
+        level_columns = int(columns/level)
+        level_rows = int(rows/level)
+
+        row = np.zeros(level_rows)
+
+        for i in range(level_rows):
+            for j in range(row.shape[0]):
+                row[j] = im[i][j]
+
+            haar(row)
+
+            for j in range(row.shape[0]):
+                if row[j] < 0:
+                    im[i][j] = 127
+                else:
+                    im[i][j] = row[j]
+
+        column = np.zeros(level_columns)
+        for j in range(level_columns):
+            for i in range(column.shape[0]):
+                column[i] = im[i][j]
+
+            haar(column)
+
+            for i in range(column.shape[0]):
+                if column[i] < 0:
+                    im[i][j] = 127
+                else:
+                    im[i][j] = column[i]
+
+
+def scale(fromMin, fromMax, toMin, toMax, x):
+        if fromMax - fromMin == 0:
+            return 0
+
+        value = ((toMax - toMin) * (x - fromMin)) / ((fromMax - fromMin) + toMin)
+
+        if value > toMax:
+            value = toMax
+
+        if value < toMin:
+
+            value = toMin
+
+        return value
